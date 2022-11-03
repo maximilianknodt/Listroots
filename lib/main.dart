ab2de6e654eadd82d9a7b2da83ca8236f1120919
chrhuese <christoph.huesemann@hs-osnabrueck.de>
2022-11-03 22:19:50 +0100

    Themes für Light- und Darkmode erstellt

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,28 @@ void main() {
   runApp(const MyApp());
 }
 
+// fragt nicht. Das ist super dumm
+// https://stackoverflow.com/questions/62432229/how-to-assign-hexadecimal-color-code-in-primaryswatch-in-flutter
+MaterialColor buildMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map<int, Color> swatch = {};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,16 +35,86 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+          brightness: Brightness.light,
+          primarySwatch: buildMaterialColor(const Color(0xFF5F9220)),
+          primaryColor: const Color(0xFF5F9220),
+          canvasColor: const Color(0xFFFDFEFC),
+          backgroundColor: const Color(0xFFF5F5F4),
+          cardColor: const Color(0XFFEBEBEA), //Primary-Container
+          fontFamily: 'IBM Plex Sans',
+          textTheme: const TextTheme(
+            //Font-Weight class https://api.flutter.dev/flutter/dart-ui/FontWeight-class.html
+            //w600 = semi-bold, w500 = medium, w400 = regular
+            // Text-secondary fehlt noch
+            titleLarge: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFF181F0F)),
+            titleMedium: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFF181F0F)),
+            titleSmall: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFF181F0F)),
+            displayLarge: TextStyle(
+                fontSize: 57,
+                fontFamily: 'Swistblnk Monthoers',
+                color: Color(0xFF181F0F)),
+            displayMedium: TextStyle(
+                fontSize: 45,
+                fontFamily: 'Swistblnk Monthoers',
+                color: Color(0xFF181F0F)),
+            displaySmall: TextStyle(
+                fontSize: 36,
+                fontFamily: 'Swistblnk Monthoers',
+                color: Color(0xFF181F0F)),
+            labelLarge: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF181F0F)),
+            labelMedium: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF181F0F)),
+            labelSmall: TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF181F0F)),
+            bodyLarge: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF181F0F)),
+            bodyMedium: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF181F0F)),
+            bodySmall: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF181F0F)),
+            headlineLarge: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF181F0F)),
+            headlineMedium: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF181F0F)),
+            headlineSmall: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF181F0F)),
+          )),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: buildMaterialColor(const Color(0xFF5F9220)),
+        primaryColor: const Color(0xFF5F9220),
+        canvasColor: const Color(0xFF1F221A),
+        backgroundColor: const Color(0xFF141611),
+        cardColor: const Color(0xFF29341D),
+        fontFamily: 'IBM Plex Sans',
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
