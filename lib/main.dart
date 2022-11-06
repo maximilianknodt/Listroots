538e148483f736c765c48e8ffe6bce6d1fbf3a90
chuesemann <christoph.huesemann@osnanet.de>
2022-11-06 01:16:58 +0100

    Color-Scheme fertiggestellt

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: buildMaterialColor(const Color(0xFF5F9220)),
-          primaryColor: const Color(0xFF5F9220),
-          canvasColor: const Color(0xFFFDFEFC),
-          backgroundColor: const Color(0xFFF5F5F4),
-          cardColor: const Color(0XFFEBEBEA), //Primary-Container
+          colorScheme: const ColorScheme(
+              brightness: Brightness.light,
+              primary: Color(0xFF5F9220), //Primärfarbe
+              secondary: Color(0xFF181F0F), // Text-Farbe
+              tertiary: Color(
+                  0xC0181F0F), //sekundäre Text-Farbe (Text-Farbe 75%(0xC0 alpha))
+              error: Color(0xFFFF0000),
+              onError: Color(0xFFFFFFFF),
+              background: Color(0xFFF5F5F4),
+              primaryContainer: Color(0xFFEBEBEA),
+              surface: Color(0XFFEBEBEA), // --> Canvas
+              onSurface: Color(0xFF181F0F),
+              //nur da, weil required
+              onPrimary: Color(0xFFFFFFFF),
+              onBackground: Color(0xFFFFFFFF),
+              onSecondary: Color(0xFFFFFFFF)),
           fontFamily: 'IBM Plex Sans',
           textTheme: const TextTheme(
             //Font-Weight class https://api.flutter.dev/flutter/dart-ui/FontWeight-class.html
@@ -108,12 +118,22 @@ class MyApp extends StatelessWidget {
                 color: Color(0xFF181F0F)),
           )),
       darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: buildMaterialColor(const Color(0xFF5F9220)),
-        primaryColor: const Color(0xFF5F9220),
-        canvasColor: const Color(0xFF1F221A),
-        backgroundColor: const Color(0xFF141611),
-        cardColor: const Color(0xFF29341D),
+        colorScheme: const ColorScheme(
+            brightness: Brightness.dark,
+            primary: Color(0xFF5F9220), // Primärfarbe
+            secondary: Color(0xFFF5F5F4), // Text-Farbe
+            tertiary:
+                Color(0xC0F5F5F4), // sekundäre Text-Farbe (Text-Farbe 75%)
+            error: Color(0xFFFF0000),
+            onError: Color(0xFFFFFFFF),
+            background: Color(0xFF141611),
+            primaryContainer: Color(0xFF29341D),
+            surface: Color(0xFF1F221A), // --> Canvas
+            onSurface: Color(0xFFF5F5F4),
+            //nur da, weil required
+            onPrimary: Color(0xFFFFFFFF),
+            onBackground: Color(0xFFFFFFFF),
+            onSecondary: Color(0xFFFFFFFF)),
         fontFamily: 'IBM Plex Sans',
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
