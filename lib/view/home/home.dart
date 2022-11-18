600b13eb2c406cb65cdf534cd4cd82b06cce692c
Divinythor <thorbenfab@yahoo.de>
2022-11-18 18:53:53 +0100

    Fertigstellung der Stats-Card
    LS-137 Fertigstellung der Stats-Card
    Fragen:
    - Komponenten alle in einer File pro Screen oder Aufteilen auf viele Files?
    - Wenn viele Files, wie in den entsprechenden Screen einbinden?
    - Bessere Möglichkeit, die ganzen Container nutzen zu müssen?

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,3 +1,5 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:latlng/latlng.dart';
 
@@ -8,31 +10,72 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        height: 300,
-        child: RecordedTripMap(
-          start: DateTime.now(),
-          end: DateTime.now()..subtract(const Duration(hours: 1)),
-          path: const [
-            LatLng(52.27262175883876, 8.04809522538859),
-            LatLng(52.272920574108596, 8.049362430692437),
-            LatLng(52.27411581504878, 8.050543095179428),
-            LatLng(52.27454321934981, 8.049671505173729),
-            LatLng(52.27508787016352, 8.04886791156641),
-            LatLng(52.2744902668304, 8.047260724351775),
-            LatLng(52.27457726022175, 8.046852746058828),
-            LatLng(52.274599954121825, 8.046413860319445),
-            LatLng(52.274516743098005, 8.046389134362299),
-            LatLng(52.27396452052699, 8.04510338459059),
-          ],
-          detections: const [
-            LatLng(52.27477079208176, 8.048024621625713),
-            LatLng(52.274955037472004, 8.048464251735915),
-            LatLng(52.27341103744562, 8.0498373429437715),
-          ],
+    return Column(
+      children: [
+        Center(
+          child: SizedBox(
+            height: 300,
+            child: RecordedTripMap(
+              start: DateTime.now(),
+              end: DateTime.now()..subtract(const Duration(hours: 1)),
+              path: const [
+                LatLng(52.27262175883876, 8.04809522538859),
+                LatLng(52.272920574108596, 8.049362430692437),
+                LatLng(52.27411581504878, 8.050543095179428),
+                LatLng(52.27454321934981, 8.049671505173729),
+                LatLng(52.27508787016352, 8.04886791156641),
+                LatLng(52.2744902668304, 8.047260724351775),
+                LatLng(52.27457726022175, 8.046852746058828),
+                LatLng(52.274599954121825, 8.046413860319445),
+                LatLng(52.274516743098005, 8.046389134362299),
+                LatLng(52.27396452052699, 8.04510338459059),
+              ],
+              detections: const [
+                LatLng(52.27477079208176, 8.048024621625713),
+                LatLng(52.274955037472004, 8.048464251735915),
+                LatLng(52.27341103744562, 8.0498373429437715),
+              ],
+            ),
+          ),
         ),
-      ),
+        Container(
+          padding: EdgeInsets.only(left: 25, top: 45),
+          child: Row(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 12),
+                width: 174,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(children: [
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.only(left: 12),
+                    child: Text(
+                      '42',
+                      style: Theme.of(context).textTheme.displayLarge,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Divider(
+                    thickness: 1,
+                    indent: 10,
+                    endIndent: 10,
+                  ),
+                  Text(
+                    'Erkennungen von Schäden (insgesamt)',
+                    style: Theme.of(context).textTheme.bodySmall,
+                    textAlign: TextAlign.center,
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/view/home/stats_card.dart
+++ lib/view/home/stats_card.dart
@@ -1,10 +1,25 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class StatsCard extends StatelessWidget {
-  const StatsCard({super.key});
+// class StatsCard extends StatelessWidget {
+//   const StatsCard({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [Card()]);
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       alignment: Alignment.centerLeft,
+//       decoration: BoxDecoration(
+//         color: Theme.of(context).colorScheme.surface,
+//         borderRadius: BorderRadius.circular(10),
+//       ),
+//       child: Column(children: [Row(children: [])]),
+//     );
+//   }
+// }
+
+/**
+ * 
+ * Zu klären: Lieber einzelne files 
+ * für jede Komponente eines Screens 
+ * oder 1 File pro Screen
+ * 
+ */
