7cb91e6a989fd969d02f877ca1188c6cea1eee83
chrhuese <christoph.huesemann@hs-osnabrueck.de>
2022-11-19 23:14:35 +0100

    LS-140 Text, Icon and Alignment changed

--- lib/logic/archive/listElement.dart
+++ lib/logic/archive/listElement.dart
@@ -12,15 +12,29 @@ class ListElement extends StatelessWidget {
             children: <Widget>[
               Column(
                 mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Koordinaten"),
-                  Text("Uhrzeit + Konfidenz")
+                  Padding(
+                    padding: EdgeInsets.only(left: 5, top: 2),
+                    child: Text(
+                      "52° 14'271\" | 008° 16'291\"",
+                      style: Theme.of(context).textTheme.bodySmall,
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(left: 5, bottom: 2),
+                      child: Text(
+                        "13:42 Uhr, 40% Konfidenz",
+                        style: Theme.of(context).textTheme.labelSmall,
+                        textAlign: TextAlign.start,
+                      ))
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 mainAxisSize: MainAxisSize.min,
-                children: <Widget>[Icon(Icons.gps_fixed_outlined)],
+                children: <Widget>[Icon(Icons.pin_drop_outlined)],
               )
             ]),
       ),
