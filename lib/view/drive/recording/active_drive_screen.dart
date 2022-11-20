ac89625230aab771de63adc1087170d5deed856e
Max <maximilian.knodt@hs-osnabrueck.de>
2022-11-20 16:37:45 +0100

    LS-159 RecordingDisplay & SensorMeter implemented

--- lib/view/drive/recording/active_drive_screen.dart
+++ lib/view/drive/recording/active_drive_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:intl/intl.dart';
 import 'package:listroots/view/widgets/map/static_map.dart';
 
 class ActiveDriveScreen extends StatelessWidget {
@@ -22,19 +23,41 @@ class ActiveDriveScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          // TODO: RecordingDisplay
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: RecordingDisplay(
+              duration: "Dauer als Zeit",
+              timestamp: DateTime.now(),
+            ),
+          ),
           Expanded(
             // Frage: Warum muss hier auf einmal Expanded hin, damit kein Render Fehler auftritt? 'RenderBox was not laid out'
             child: Stack(
               children: <Widget>[
-                //StaticMap(),
+                StaticMap(),
+                Container(
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.center,
+                      colors: <Color>[
+                        Theme.of(context).colorScheme.background,
+                        Theme.of(context)
+                            .colorScheme
+                            .background
+                            .withOpacity(0.0),
+                      ],
+                    ),
+                  ),
+                ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  padding: const EdgeInsetsDirectional.fromSTEB(25, 10, 25, 10),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
                       AspectRatio(
-                        aspectRatio: 4 / 3,
+                        aspectRatio: 5 / 3, // TODO: Bildgroesse klaeren
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(10),
                           child: Image.asset(
@@ -43,7 +66,10 @@ class ActiveDriveScreen extends StatelessWidget {
                           ),
                         ),
                       ),
-                      // TODO: SensorMeter
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5.0),
+                        child: SensorMeter(rash: 0.7),
+                      ),
                       Spacer(),
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 20),
@@ -67,6 +93,91 @@ class ActiveDriveScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class RecordingDisplay extends StatelessWidget {
+  RecordingDisplay({
+    super.key,
+    required this.duration,
+    required this.timestamp,
+  });
+
+  String duration; //TODO: Typ und Umwandlungimplementierung aendern
+  final DateTime timestamp;
+
+  @override
+  Widget build(BuildContext context) {
+    DateFormat time = DateFormat(DateFormat.HOUR24_MINUTE);
+    String strTime = time.format(timestamp);
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        //TODO: RecordingDot Anstelle des Icons
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Icon(
+            Icons.lens,
+            color: Colors.red,
+            size: 20,
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              AppLocalizations.of(context)!.recordingActive,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Text(
+              "$duration (" +
+                  AppLocalizations.of(context)!.startedAt +
+                  " $strTime Uhr)",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class SensorMeter extends StatelessWidget {
+  SensorMeter({
+    super.key,
+    this.rash = 0.0,
+  });
+
+  double rash;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              width: 4,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        FractionallySizedBox(
+          widthFactor: rash,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                width: 4,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
