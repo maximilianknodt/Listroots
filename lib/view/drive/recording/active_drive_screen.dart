19908c0807ce63adee321977770b2d9051230152
Jannis <87816121+jannisgaertner@users.noreply.github.com>
2022-11-21 13:42:41 +0100

    LS-159 split widgets into multiple files

--- lib/view/drive/recording/active_drive_screen.dart
+++ lib/view/drive/recording/active_drive_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
-import 'package:intl/intl.dart';
-import 'package:listroots/view/widgets/map/static_map.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../widgets/map/static_map.dart';
+import 'recording_display.dart';
+import 'sensor_meter.dart';
 
 class ActiveDriveScreen extends StatelessWidget {
   const ActiveDriveScreen({super.key});
@@ -92,95 +94,11 @@ class ActiveDriveScreen extends StatelessWidget {
     );
   }
 
-  void _savePosition() {}
-
-  void _stopRide() {}
-}
-
-extension DurationFormat on Duration {
-  String format() => this.toString().split('.').first.padLeft(8, "0");
-}
-
-class RecordingDisplay extends StatelessWidget {
-  const RecordingDisplay({
-    super.key,
-    required this.duration,
-    required this.timestamp,
-  });
-
-  final Duration duration;
-  final DateTime timestamp;
-
-  @override
-  Widget build(BuildContext context) {
-    DateFormat time = DateFormat(DateFormat.HOUR24_MINUTE);
-    String strTime = time.format(timestamp);
-
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        //TODO: RecordingDot Anstelle des Icons
-        Padding(
-          padding: const EdgeInsets.only(right: 10),
-          child: Icon(
-            Icons.lens,
-            color: Colors.red,
-            size: 20,
-          ),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              AppLocalizations.of(context)!.recordingActive,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Text(
-              "${duration.format()} (${AppLocalizations.of(context)!.startedAt} $strTime Uhr)",
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-          ],
-        ),
-      ],
-    );
+  void _savePosition() {
+    // TODO: Implement
   }
-}
 
-class SensorMeter extends StatelessWidget {
-  const SensorMeter({
-    super.key,
-    this.rash = 0.0,
-  }) : assert(rash >= 0.0 && rash <= 1.0);
-
-  final double rash;
-  final double height = 3;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              width: height,
-            ),
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        FractionallySizedBox(
-          widthFactor: rash,
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.blue,
-                width: height,
-              ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-        ),
-      ],
-    );
+  void _stopRide() {
+    // TODO: Implement
   }
 }
