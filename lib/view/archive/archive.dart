b13d441d52ee52258c067e576e762f5ad01357c9
Jannis <87816121+jannisgaertner@users.noreply.github.com>
2022-11-22 01:01:51 +0100

    LS-136 finished off section widget

--- lib/view/archive/archive.dart
+++ lib/view/archive/archive.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:listroots/logic/archive/listElement.dart';
-import 'package:listroots/view/widgets/general/section.dart';
 
 class Archive extends StatelessWidget {
   const Archive({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Section());
+    return const Center(child: Text("Archive"));
   }
 }
--- lib/view/widgets/general/section.dart
+++ lib/view/widgets/general/section.dart
@@ -1,27 +1,58 @@
-import 'dart:developer';
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 class Section extends StatelessWidget {
   const Section({
     super.key,
-  });
+    required this.child,
+    required this.title,
+    this.leading,
+    this.trailing,
+    this.onTap,
+  }) : assert(
+          (trailing != null && onTap == null) ||
+              (trailing == null && onTap != null) ||
+              (trailing == null && onTap == null),
+        );
+
+  final Widget child;
+  final String title;
+  final Widget? leading;
+  final Widget? trailing;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: InkWell(
-      child: Container(
-        child: ListTile(
-          leading: Icon(Icons.abc),
-          title: Text("ja moin"),
-          trailing: Icon(Icons.chevron_right),
-        ),
+    return InkWell(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                if (leading != null)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: leading!,
+                  ),
+                Expanded(
+                  child: Text(
+                    title,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                if (trailing != null)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: trailing!,
+                  ),
+                if (onTap != null) const Icon(Icons.chevron_right),
+              ],
+            ),
+          ),
+          child,
+        ],
       ),
-      onTap: () {
-        print("funktionier doch bitte");
-      },
-    ));
+      onTap: onTap,
+    );
   }
 }
