79d5d579dec65d0c2477018c4bc3b01ac8e2ce46
Maximilian <maximilian.knodt@hs-osnabrueck.de>
2022-12-23 16:37:36 +0100

    LS-64 OSM Login via Credential Loading at Appstart

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:listroots/logic/auth/auth_bloc.dart';
 
+import 'data/auth/client.dart';
+import 'data/auth/oauth_main.dart';
 import 'global_bloc_provider.dart';
 import 'main.dart';
 import 'theme.dart';
@@ -23,8 +27,18 @@ class _ListrootsApp extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  void initialiseClient(BuildContext context) async {
+    OAuthMain oauth = OAuthMain();
+    bool credsExist = await oauth.credentialsExist();
+    if (credsExist) {
+      Client client = await oauth.loadClient();
+      BlocProvider.of<AuthBloc>(context).add(AuthEventAtStart(client));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    initialiseClient(context);
     return MaterialApp.router(
       title: 'Listroots',
       theme: AppColorTheme().theme,
--- lib/view/home/info_button.dart
+++ lib/view/home/info_button.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../logic/auth/auth_bloc.dart';
 
 class InfoButton extends StatefulWidget {
   const InfoButton({super.key});
@@ -10,9 +13,21 @@ class InfoButton extends StatefulWidget {
 class _InfoButtonState extends State<InfoButton> {
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {},
-      icon: const Icon(Icons.info_outlined),
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        return IconButton(
+          onPressed: () {}, // TODO: Was auch immer hier passieren sollte
+          icon: state is Authenticated
+              ? Text(
+                  state.client.initials,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(color: Theme.of(context).colorScheme.primary),
+                )
+              : const Icon(Icons.error_outline),
+        );
+      },
     );
   }
 }
